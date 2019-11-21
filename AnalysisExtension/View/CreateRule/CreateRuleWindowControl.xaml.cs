d278403f820662f4dbff33705043a451da64803e
nori243 <bcat243@gmail.com>
2019-11-21 16:32:53 +0800

    add base UI into CreateRuleToolWindow

--- AnalysisExtension/View/CreateRule/CreateRuleWindowControl.xaml.cs
+++ AnalysisExtension/View/CreateRule/CreateRuleWindowControl.xaml.cs
@@ -1,34 +1,49 @@
 ﻿namespace AnalysisExtension
 {
+    using System;
+    using System.ComponentModel;
     using System.Diagnostics.CodeAnalysis;
+    using System.Drawing;
+    using System.Drawing.Design;
     using System.Windows;
     using System.Windows.Controls;
 
     /// <summary>
     /// Interaction logic for ToolWindow1Control.
     /// </summary>
-    public partial class ToolWindow1Control : UserControl
+    public partial class CreateRuleToolWindow1Control : UserControl
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ToolWindow1Control"/> class.
+        /// Initializes a new instance of the <see cref="CreateRuleToolWindow1Control"/> class.
         /// </summary>
-        public ToolWindow1Control()
+
+        public CreateRuleToolWindow1Control()
         {
             this.InitializeComponent();
+            SizeChanged += OnSizeChanged;
         }
 
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "ToolWindow1");
+            double newWindowHeight = e.NewSize.Height;
+            double newWindowWidth = e.NewSize.Width;
+            int padding = 20;
+            ruleCreateStackPanel.Width = newWindowWidth - 60;
+            ruleCreateStackPanel.Height = newWindowHeight - padding;
+            ruleBefore.Height = ruleCreateStackPanel.Height / 2 - padding;
+            ruleBefore.Width = ruleCreateStackPanel.Width - padding;
+            ruleAfter.Height = ruleCreateStackPanel.Height / 2 - padding;
+            ruleAfter.Width = ruleCreateStackPanel.Width - padding;
+        }
+
+        private void RuleBefore_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            StaticValue.OnPreviewMouseWheelListener(sender,e);
+        }
+
+        private void OnClickBtCancelListener(object sender, RoutedEventArgs e)
+        {
+            StaticValue.CloseWindow(this);
         }
     }
 }
